12384db57bbaf909b603e5ee0b00f97515caeb53
Artem <1v@users.noreply.github.com>
2016-11-08 08:42:05 +0300

    Wow it is working

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -9,13 +9,8 @@ begin
     di.install "activesupport", ">= 4.0.0"
   end
 rescue => e
-  warn "#{$0}: #{e}"
-
   exit!
 end
-
-puts "Writing fake Rakefile"
-
 # Write fake Rakefile for rake since Makefile isn't used
 File.open(File.join(File.dirname(__FILE__), 'Rakefile'), 'w') do |f|
   f.write("task :default" + $/)
